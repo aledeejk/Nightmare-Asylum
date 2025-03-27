731dc44b190e94e362ad69a14965b2b53c0490bf
aledeejk <aledee@sfedu.ru>
2025-03-28 01:38:15 +0300

    menu, prolog
    добавила меню и пролог, связала сцены между собой

---
+++ NA(worked)/Assets/mainmenu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainmenu : MonoBehaviour
+{
+    public void Exit()
+    {
+        //Debug.Log("exit!");
+        Application.Quit();
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
